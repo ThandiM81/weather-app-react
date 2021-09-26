ea9ba7e800133bfce0b6ce2fc18c2b3872873054
ThandiM81 <thandiwe.mtetwa@googlemail.com>
2021-09-26 15:51:45 +0100

    Installed bootstrap

--- src/Form.js
+++ src/Form.js
@@ -5,34 +5,32 @@ import "./form.css";
 export default function Form() {
   return (
     <div className="Form">
-      <form>
-        <div className="row">
-          <div className="col-md-7 ">
-            <input
-              type="search"
-              placeholder="Enter city...."
-              class="form-control"
-              id="city-entry"
-              autocomplete="off"
-              autofocus="on"
-            />
-          </div>
-          <div className="col-md-4 p-1" id="find">
-            <input
-              type="submit"
-              className="search-button"
-              value="Search 🔍"
-              id="search-city"
-            />
-            <input
-              type="submit"
-              className="location-button"
-              value="Current 📍"
-              id="location-search"
-            />
-          </div>
+      <div className="row">
+        <div className=" col-8">
+          <input
+            type="search"
+            placeholder="Enter city...."
+            id="city-entry"
+            className="form-control"
+            autocomplete="off"
+            autofocus="on"
+          />
         </div>
-      </form>
+        <div className="col" id="find">
+          <input
+            type="submit"
+            className="search-button"
+            value="Search 🔍"
+            id="search-city"
+          />
+          <input
+            type="submit"
+            className="location-button"
+            value="Current 📍"
+            id="location-search"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
 
 import WeatherApp from "./WeatherApp";
 
